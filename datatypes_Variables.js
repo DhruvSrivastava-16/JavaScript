27f1fcd0ed1345203d9e5768fc17eff1e4840e44
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-05-22 22:02:55 -0700

    Add files via upload

---
+++ datatypes_Variables.js
@@ -0,0 +1,20 @@
+let num = 2;
+let bool = true;
+let c = "ferrari is amazing";
+let f = 14.55
+let arr = [4,5,6,"dhruv"]
+let obj = {               //Object Declaration in JS 
+    a: 100,
+    b: "Hey!"
+}
+
+console.log(typeof c);
+c = 44;
+console.log(typeof c);       //Variables dont have a particular type but the value they take can have a type
+
+console.log(typeof f);
+
+console.log(typeof arr);
+
+console.log(obj)
+console.log(obj.a)
