ff41f9a8b459dd79cf8f5fd3096fba6889c4a37c
DhruvSrivastava-16 <67152185+DhruvSrivastava-16@users.noreply.github.com>
2021-05-22 22:02:36 -0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,16 @@
+let a = 10;
+
+for(let i =0; i<a;i++)
+{
+    console.log(i)
+}
+
+//printing this in 1 line
+
+let result = "";
+
+for(let i=0;i<a;i++)
+{
+    result += i+" ";
+}
+console.log(result)
---
+++ syntax.js
@@ -0,0 +1,16 @@
+//single line comment
+
+/* 
+multi
+line
+comment
+*/
+
+var a = 10;   //; are not compulsory 
+var b = 10
+
+let c = 40;
+const d = 55;
+
+c++;
+console.log(c);
